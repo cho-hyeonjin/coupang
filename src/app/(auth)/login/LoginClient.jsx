8bb56b59def71581b364022013ceb5a0cdb37f04
cho-hyeonjin <gusls2005@gmail.com>
2024-03-08 23:48:31 +0900

    feat: Input 컴포넌트 생성

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 
 import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
+import Input from "@/components/input/Input";
 
 const LoginClient = () => {
   const [email, setEmail] = useState();
@@ -39,7 +40,29 @@ const LoginClient = () => {
 
           <form onSubmit={loginUser} className={styles.form}>
             {/* Input */}
-            Input
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              vlaue={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              vlaue={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className={styles.group}>
               {/* 자동 로그인, 비밀번호 수정 */}
               자동 로그인, 비밀번호 수정
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Input.module.scss";
+import classNames from "classnames";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({
+  id,
+  label,
+  name = "",
+  labelVisible,
+  icon,
+  email,
+  password,
+  placeholder = "",
+  readOnly,
+  disabled,
+  value,
+  error: errorProp, //  이런 식으로 쓰면 errorProp이름으로 error prop을 사용할 수 있음
+  className = "",
+  onChange,
+  ...restProps
+}) => {
+  const [inputValue, setInputValue] = useState(value ? value : "");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const checkType = () => {
+    if (email) {
+      return "email";
+    }
+
+    if (password) {
+      return isPasswordVisible ? "text" : password;
+    }
+
+    return "text";
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+    onChange(e);
+  };
+
+  const iconType = isPasswordVisible ? "show" : "hide";
+  const iconLabel = `비밀번호 ${isPasswordVisible ? "표시" : "감춤"}`;
+
+  return (
+    // className={classNames(styles.foramControl, className)} 은 라이브러리 사용한 문법.
+    // classNames=('test1', 'test2') → className='test1 test2'
+    <div className={classNames(styles.foramControl, className)}>
+      <label
+        htmlFor={id}
+        className={classNames(styles.label, labelVisible || styles.labelHidden)}
+      >
+        {label}
+      </label>
+
+      <div
+        className={classNames(
+          styles.inputWrapper,
+          errorProp && styles.inputWrapperError
+        )}
+      >
+        {/* {icon ? <Icon /> : null} */}
+        <input
+          id={id}
+          type={checkType()}
+          name={name}
+          className={styles.input}
+          placeholder={placeholder}
+          readOnly={readOnly}
+          disabled={disabled}
+          value={inputValue}
+          onChange={handleChange}
+          {...restProps}
+        />
+
+        {password ? (
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => setIsPasswordVisible((prev) => !prev)}
+            disabled={disabled}
+            icon={icon}
+          >
+            {/* <Icon type={iconType} alt={iconLable} title={iconLable} /> */}
+          </button>
+        ) : null}
+      </div>
+      {errorProp && (
+        <span role="alert" className={styles.error}>
+          {errorProp.message}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Input;
