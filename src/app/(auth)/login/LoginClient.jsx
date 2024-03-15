b50bae9daec0659a60bf9aed1c25080e794b701d
cho-hyeonjin <gusls2005@gmail.com>
2024-03-15 23:01:22 +0900

    feat: Button 컴포넌트 생성

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -11,6 +11,8 @@ import Loader from "@/components/loader/Loader";
 import Input from "@/components/input/Input";
 import AutoSignInCheckbox from "@/components/autoSignInCheckbox/AutoSignInCheckbox";
 import Divider from "@/components/divider/Divider";
+import Button from "@/components/button/Button";
+import Link from "next/link";
 
 const LoginClient = () => {
   const [email, setEmail] = useState();
@@ -76,13 +78,17 @@ const LoginClient = () => {
 
           <div className={styles.buttonGroup}>
             {/* Button */}
-            Button
+            <Button type="submit" width="100%">
+              로그인
+            </Button>
             <Divider />
-            Button
+            <Button type="submit" width="100%" secondary>
+              <Link href={"/register"}>회원가입</Link>
+            </Button>
             <Divider />
             <div>
               {/* Button */}
-              Button
+              <Button onClick={signInWithGoogle}>구글 로그인</Button>
             </div>
           </div>
         </div>
