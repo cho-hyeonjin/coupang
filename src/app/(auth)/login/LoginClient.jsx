b76eafa938a7552d51d21534221b94cd39641bf1
cho-hyeonjin <gusls2005@gmail.com>
2024-03-15 22:43:49 +0900

    feat: Divider 컴포넌트 생성

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -10,6 +10,7 @@ import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
 import Input from "@/components/input/Input";
 import AutoSignInCheckbox from "@/components/autoSignInCheckbox/AutoSignInCheckbox";
+import Divider from "@/components/divider/Divider";
 
 const LoginClient = () => {
   const [email, setEmail] = useState();
@@ -76,6 +77,9 @@ const LoginClient = () => {
           <div className={styles.buttonGroup}>
             {/* Button */}
             Button
+            <Divider />
+            Button
+            <Divider />
             <div>
               {/* Button */}
               Button
